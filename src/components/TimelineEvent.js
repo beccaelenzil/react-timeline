b038252afbc198179fbda8f3215700e5eb6b8e2c
beccaelenzil <beccaelenzil@gmail.com>
2019-11-19 11:06:48 -0800

    timelineEvent css - still needs a little refinement

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -6,10 +6,8 @@ const TimelineEvent = (props) => {
   // Fill in your code here
   return(
     <div className="timeline-event">
-      <p>
-        <span className="event-person">{props.person}</span>
-        <span className="event-time"><Timestamp time={props.timestamp}/></span>
-      </p>
+      <span className="event-person">{props.person}</span>
+      <span className="event-time"><Timestamp time={props.timestamp}/></span>
       <p className="event-status">{props.status}</p>
     </div>
   );
